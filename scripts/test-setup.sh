54f4d235a49238252723f1184715b21a5990f5d1
Michael Ngo <mike.ngo@automatic.com>
2018-05-15 14:56:56 -0700

    Clean up scripts

--- scripts/test-setup.sh
+++ scripts/test-setup.sh
@@ -2,21 +2,7 @@
 
 set -x
 
-if [[ "${TRAVIS}" == "true" ]]; then
-    go get github.com/kardianos/govendor
-
-    go tool vet .
-
-    gofmt -d .
-    if [ "$?" -ne "0" ]; then
-        echo "Code not properly formatted. Run 'go fmt'"
-        exit 1
-    fi
-
-    govendor sync
-
-    if [ ! -z "$(govendor list -no-status +outside)" ]; then
-        echo "External dependencies found, only vendored dependencies supported"
-        exit 1
-    fi
+if [ ! -z "$(govendor list -no-status +outside)" ]; then
+    echo "External dependencies found, only vendored dependencies supported"
+    exit 1
 fi
